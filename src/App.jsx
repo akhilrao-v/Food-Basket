a82547bbf19e82c7aec727e257ee6d654e6ffd19
akhilrao-v <raoakhil1999@gmail.com>
2025-11-21 12:58:03 +0530

    version 1.1

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar/Navbar.jsx';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home.jsx';
 import Cart from './pages/Cart/Cart.jsx';
-import PlaceOrder from './pages/Placeorder/Placeorder.jsx';
+import PlaceOrder from './pages/Placeorder/PlaceOrder.jsx';
 import Footer from './components/Footer/Footer.jsx';
 import LoginPopup from './components/LoginPopup/LoginPopup.jsx';
 
